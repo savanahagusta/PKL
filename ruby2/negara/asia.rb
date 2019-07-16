5b0e62696fada5150b2df0f9d83b2f91f209d62d
savanahagusta <52643117+savanahagusta@users.noreply.github.com>
2019-07-16 23:14:31 +0800

    Update asia.rb
    mengganti nama class, method, variable, penggunaan hash, serta merapikan penulisan code.

--- ruby2/negara/asia.rb
+++ ruby2/negara/asia.rb
@@ -1,35 +1,39 @@
-class As
-  def sia
-    negara = {Indonesia: 'Asia Tenggara', India: 'Asia Selatan', Korea: 'Asia Timur' , Turki:'Asia Barat'}
-    p = 0
-    while p !=5
-      puts "============================"
-      puts "Negara --> Wilayah Benua"
-      puts "============================"
-      puts "1. Indonesia"
-      puts "2. India"
-      puts "3. Korea"
-      puts "4. Turki"
-      puts "5. Keluar" 
-      puts "============================"
-      print "Masukan Pilihan Anda: "
-      p = gets.to_i
-      puts "============================"
-		
-		  if p == 1
-			  puts negara[:Indonesia] 
-		  elsif p == 2
-			  puts negara[:India]
-		  elsif p == 3
-			  puts negara[:Korea]
-		  elsif p == 4
-			  puts negara[:Turki]
-		  elsif p == 5
-			  load "main.rb"
-		  else
-			  puts "Anda Salah Memasukan Angka !!"
-			  exit
-      end
-    end
+class Asia
+	def asia
+		load "negara.rb"
+		n = Negara.new
+    negara = { 
+			country: [
+				{negara: 'Indonesia', wilayah: 'Asia Tenggara'},
+        {negara: 'India', wilayah: 'Asia Selatan'},
+        {negara: 'Korea', wilayah: 'Asia Timur'},
+				{negara: 'Turki', wilayah: 'Asia Barat'}
+			]
+		}
+		puts "-------------------------------------------"
+		puts "  Negara di Asia  "
+		k=1
+    negara[:country].each do |list| 
+				puts "#{k}. #{list.fetch(:negara)}"
+				k=k+1
+		end
+		puts "(mencari wilayah benua salah satu negara)"
+		puts "wilayah/kembali/keluar"
+		b = gets.chomp
+		if b == "wilayah"
+			m=1
+    	negara[:country].each do |list| 
+				puts "#{m}. Negara #{list.fetch(:negara)} berada di Wilayah #{list.fetch(:wilayah)}"
+			m=m+1
+			end
+			n.negara
+		elsif b == "kembali"
+			n.negara
+		elsif b == "keluar"
+			exit
+		else
+			puts "anda salah memasukan inputan !"
+			exit
+		end
 	end
 end
