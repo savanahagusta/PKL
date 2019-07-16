8d85149a1bc8eadc7760838843febe79ce531061
savanahagusta <52643117+savanahagusta@users.noreply.github.com>
2019-07-16 23:30:22 +0800

    Update negara.rb
    mengganti nama class, method, variabel, mengubah cara penulisan array dan pemanggilan array, merapikan penulisan code.

--- ruby2/negara/negara.rb
+++ ruby2/negara/negara.rb
@@ -1,54 +1,37 @@
-class Neg
-	def gara
-		load "asia.rb"
-		load "eropa.rb"
-		asy = ["Indonesia", "India", "Korea", "Turki"]
-		err = ["Finlandia","Italia","Estonia","Jerman"]
-		aff = ["Kenya","Mesir","Ghana","Angola"]
-		amr = ["Kanada","Meksiko","Guatemala","Argentina"]
-		
-		p = 0
-    while p !=5
-			puts "============================"
-			puts "Benua --> Negara"
-			puts "============================"
-			puts "1. Asia"
-			puts "2. Eropa"
-			puts "3. Afrika"
-			puts "4. Amerika"
-			puts "5. Keluar" 
-			puts "============================"
-			print "Masukan Pilihan Anda: "
-			pilih = gets.to_i
-
-			if pilih == 1
-				puts "Negara di Asia"
-				for x in asy
-					puts "--> #{x}"
-				end
-				As.new.sia
-			elsif pilih == 2
-				puts "Negara di Eropa"
-				for x in err
-					puts "--> #{x}"
-				end
-				Er.new.rop
-			elsif pilih == 3
-				puts "Negara di Afrika"
-				for x in aff
-					puts "--> #{x}"
-				end
-			elsif pilih == 4
-				puts "Negara di Amerika"
-				for x in amr
-					puts "--> #{x}"
-				end
-			elsif pilih == 5
-				exit
-			else
-				"Anda Salah Memasukan Inputan !"
-				exit
-			end
-		end
-	end
+class Negara
+  def negara
+    load "asia.rb"
+    load "eropa.rb"
+    a = Asia.new
+    e = Eropa.new
+    n = Negara.new
+    arr = [
+      ["Asia","Eropa","Afrika","Amerika"],
+      ["Kenya","Mesir","Ghana","Angola"],
+      ["Kanada","Meksiko","Guatemala","Argentina"]
+    ]
+    puts "-------------------------------------------"
+    puts "Benua yang ada di Bumi"
+    puts arr[0]
+    puts "pilih: asia/eropa/afrika/amerika/keluar"
+    b = gets.chomp
+    if b == "asia"
+      a.asia
+    elsif b == "eropa"
+      e.eropa
+    elsif b == "afrika"
+      puts "Negara di Afrika"
+      puts arr[1]
+      n.negara
+    elsif b == "amerika"
+      puts "Negara di Amerika"
+      puts arr[2]
+      n.negara
+    elsif b == "keluar"
+      exit
+    else
+      puts "anda salah memasukan inputan !"
+      exit
+    end
+  end
 end
